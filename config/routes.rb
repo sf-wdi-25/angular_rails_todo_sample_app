e957cec159eec36ce4935f951b1c48d96a6080cc
tgaff <tgaff@alumni.nd.edu>
2016-02-12 16:34:51 -0800

    add routes for rails todo api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-
   root 'site#index'
-  get '*path*', to: 'site#index'
 
+
+  namespace :api, defaults: { format: :json } do
+    resources :todos, except: [:new, :edit]
+  end
+
+
+  get '*path*', to: 'site#index'
 end
