fb28e381b71ae80ff39ab6db1182408246a97086
tgaff <tgaff@alumni.nd.edu>
2016-02-12 16:14:58 -0800

    front-end controller and router configured

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,1 +1,29 @@
-angular.module("SampleApp", ['ui.router', 'templates']);
+angular.module("SampleApp", ['ui.router', 'templates'])
+  .config(routerConfig)
+  .controller('HomeController', HomeController);
+
+
+
+// Your router
+config.$inject = ['$stateProvider', '$urlRouterProvider']; // minification protection
+function routerConfig(   $stateProvider,   $urlRouterProvider) {
+  // for any unmatched URL redirect to /
+  $urlRouterProvider.otherwise("/");
+  console.log('router');
+
+  $stateProvider
+  .state('home', {
+    url: "/",
+    templateUrl: 'home.html',
+    controller: 'HomeController',
+    controllerAs: 'home'
+  });
+
+}
+
+
+function HomeController () {
+  var vm = this;
+  console.log("Hi, I'm the HomeController");
+  vm.homeTest = "Welcome to the homepage!";
+}
